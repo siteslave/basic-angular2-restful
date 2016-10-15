711c439a1da2d8d2e8fce21eb6927a7dac978e20
Satit Rianpit <rianpit@gmail.com>
2016-10-16 00:06:58 +0700

    add moment and 3rd party module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,9 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hello world!';
   constructor() {
-   
-  }
-
-
-  
+   console.log(moment().format('YYYY-MM-DD'))
+  } 
 }
